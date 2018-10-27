ed5b05584a7e95e1a16041c48b595031c07baa19
ts <31186481+tomassams@users.noreply.github.com>
2018-10-27 17:08:19 +0200

    begin adding bruteforce

--- main.c
+++ main.c
@@ -1,24 +1,19 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <crypt.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <crypt.h>
 
-int main(int argc, char** argv) {
+static const char PASSCHARS[] = "abcdefghikjlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890+\"#&/()=?!@$|[]|{}";
+static const int PASSCHARS_SIZE = sizeof(PASSCHARS) -1; 
+static const int MAX_LENGTH = 4;
+
+int dictionaryGuess(char* hash) {
+
+	FILE* dictionaryFile = fopen("./dictionary.txt","r");
 	
-	FILE* dictionaryFile = fopen("dictionary.txt","r");
-
-	char salt[13] = "$1$abcdefgh$"; // placeholder
-	char dictionaryWord[60]; // lets assume the words in the dictionary arent longer than this..
-	char* hash = argv[1];
-
-	// validate input
-	if(argc!=2 || strlen(hash) != 34) {
-		printf("Invalid argument. Exiting..\n");
-		return -1;
-	}
-
+	char salt[13] = "$1$abcefgh$"; // placeholder.. todo: should this be 13 or 12? works either way
+	char dictionaryWord[60];
+	
 	// extract the salt from the encrypted input hash
 	for(int i = 3; i < 11; i++) {
 		salt[i] = hash[i];
@@ -32,14 +27,71 @@ int main(int argc, char** argv) {
 
 		if(strncmp(hash, encrypted, sizeof(char)*34) == 0) {
 			printf("SUCCESS! Match found: %s\n", dictionaryWord);
-			return 0;
+			fclose(dictionaryFile);
+			return 1;
 		}
 
 	}
 
-	// cleanup
+	// no hits, cleanup and return
 	fclose(dictionaryFile);
-	//free(encrypted);
+	return -1;
+}
+
+// recursively iterate through all passchar combinations
+void iterate(char* combination, int index, char* hash) {
+
+char test[4] = "aabb";
+
+	if(index < (MAX_LENGTH - 1)) {
+		for(int i = 0; i < 2; i++) {
+			combination[index] = PASSCHARS[i];
+			iterate(combination, index + 1, hash);
+		}
+	}
+	else {
+		for(int i = 0; i < 2; i++) {
+			combination[index] = PASSCHARS[i];	
+			printf("Trying %s\n", combination);
+
+			if(strncmp(test, combination, sizeof(char)*4) == 0) {
+				printf("Found! %s\n", combination);
+				return;
+			}
+		}
+	}
+
+}
+
+
+void guessAllCombinations(char* hash) {
+	char salt[13] = "$1$abcefgh$";
+
+	for(int i = 3; i < 11; i++) {
+		salt[i] = hash[i];
+	}
+
+	char combinations[MAX_LENGTH +1];
+
+	memset(combinations, 0, MAX_LENGTH + 1);
+	iterate(combinations, 0, hash);
+}
+
+
+int main(int argc, char** argv) {
+	
+	char* hash = argv[1];
+
+	// validate input
+	if(argc!=2 || strlen(hash) != 34) {
+		printf("Invalid argument. Exiting..\n");
+		return -1;
+	}
+	
+	guessAllCombinations(hash);
+
+	//dictionaryGuess(hash);
+
 	return 0;
 
 }
