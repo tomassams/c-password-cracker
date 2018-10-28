68e0f91b528dc41e609823c598c37b66e7b89749
ts <31186481+tomassams@users.noreply.github.com>
2018-10-28 23:07:13 +0100

    more refactoring

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #define _GNU_SOURCE
-#include <crypt.h>
 
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include <crypt.h>
 #include <pthread.h>
 
 static const char PASSCHARS[] = "abcdefghikjlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890+\"#&/()=?!@$|[]|{}";
@@ -18,7 +18,6 @@ int compare_hashes(char* in_hash, char* guess) {
 	data.initialized = 0;
 
 	char salt[13] = "$1$abcdefgh$"; // placeholder, we replace abcde... in the loop below
-
 	for(int i = 3; i < 11; i++) {
 		salt[i] = in_hash[i];
 	}
@@ -28,38 +27,36 @@ int compare_hashes(char* in_hash, char* guess) {
 	return strncmp(in_hash, out_hash, sizeof(char)*34); // is 0 if they match
 }
 
-struct dict_thr_struct {
+struct d_thr_struct {
 	int thread_id;
 	FILE* dictionary_file;
-	int start_from;
-	int end_at;
 	char* hash;
 };
 
 void* dict_thread_runner(void* arg) {
 
-	struct dict_thr_struct* arg_struct = (struct dict_thr_struct*) arg;
-	char* hash = arg_struct->hash;
+	struct d_thr_struct* args = (struct d_thr_struct*) arg;
 
 	char word[60];
 
-	while(fscanf(arg_struct->dictionary_file, "%s", word) != EOF) {
-		if(found == 1) pthread_exit(0);
+	while(fscanf(args->dictionary_file, "%s", word) != EOF) {
 
-		printf("Thread %d: Trying word... %s\n",arg_struct->thread_id, word);
+		if(found == 1) pthread_exit(0); // make the other threads exit if match is found already
+
+		printf("Thread %d: Trying word... %s\n",args->thread_id, word);
  
-		if(compare_hashes(hash, word) == 0) {
-			printf("Thread %d: SUCCESS! Match found: %s\n", arg_struct->thread_id, word);
+		if(compare_hashes(args->hash, word) == 0) {
+			printf("Thread %d: SUCCESS! Match found: %s\n", args->thread_id, word);
 			found = 1;
 			pthread_exit(0);
 		}
 
 	}
 
-	pthread_exit(0); // no hits
+	pthread_exit(&found); // no hits
 }
 
-int guess_from_dictionary_threaded(char* hash, int num_threads) {
+int guess_from_dictionary(char* hash, int num_threads) {
 
 	FILE* dictionary_file = fopen("./dicts/dictionary.txt","r");
 	if(dictionary_file == NULL) {
@@ -67,16 +64,13 @@ int guess_from_dictionary_threaded(char* hash, int num_threads) {
 		return -1;
 	}
 
-	struct dict_thr_struct args[num_threads];
+	struct d_thr_struct args[num_threads];
 	pthread_t pthread_ids[num_threads];
 
 	for(int i = 0; i < num_threads; i++) {
 		args[i].thread_id = i;
 		args[i].hash = hash;
 		args[i].dictionary_file = dictionary_file;
-		args[i].start_from = 0;
-		args[i].end_at = 0;
-
 
 		pthread_attr_t attr;
 		pthread_attr_init(&attr);
@@ -89,40 +83,19 @@ int guess_from_dictionary_threaded(char* hash, int num_threads) {
 
 	fclose(dictionary_file);
 
-	return -1;
-}
+	if(found == 1) return 0;
 
-
-// dictionary search
-int guess_from_dictionary(char* hash) {
-
-	FILE* dictionary_file = fopen("./dicts/dictionary.txt","r");
-	
-	char word[60];
-
-	while(fscanf(dictionary_file, "%s", word) != EOF) {
-		printf("1Trying word... %s\n", word);
- 
-		if(compare_hashes(hash, word) == 0) {
-			printf("SUCCESS! Match found: %s\n", word);
-			fclose(dictionary_file);
-			return 0;
-		}
-
-	}
-
-	fclose(dictionary_file);
 	return -1;
 }
 
 // recursive call for bruteforce
-int guess(char* word, int char_index, char* hash, int word_length) {
+int recursive_gues(char* word, int char_index, char* hash, int word_length) {
 
 	if(char_index < word_length) {
 		for(int i = 0; i < PASSCHARS_SIZE; i++) {
 			word[char_index] = PASSCHARS[i];		
 
-			if(guess(word, char_index + 1, hash, word_length) == 0) {
+			if(recursive_gues(word, char_index + 1, hash, word_length) == 0) {
 				return 0; // we're done, stop looking
 			}
 		}
@@ -150,7 +123,7 @@ int guess_all_combinations(char* hash) {
 
 	for(int i = 0; i < MAX_WORD_LENGTH; i++) {
 		//memset(combinations, 0, MAX_WORD_LENGTH + 1);
-		if(guess(combinations, 0, hash, i) == 0) return 0; // match found
+		if(recursive_gues(combinations, 0, hash, i) == 0) return 0; // match found
 	}
 
 	return -1; // no hits
@@ -172,16 +145,15 @@ int main(int argc, char** argv) {
 		num_threads = atoi(argv[2]);
 	}
 
-	guess_from_dictionary_threaded(hash, num_threads);
-
 	// start with dictionary
-	//if(guess_from_dictionary(hash) == 0)
-	//	return 0;
+	if(guess_from_dictionary(hash, num_threads) == 0)
+		return 0;
 	
 	// nothing? then try brute force
-	//if(guess_all_combinations(hash) == 0)
-	//	return 0;
+	if(guess_all_combinations(hash) == 0)
+		return 0;
 
+	printf("No matches found!\n");
 	return -1;
 
 }
